5e9ae088d41075f3d512cb8d7c305b7ea6b951e1
Illia <ilaruslanovich7@gmail.com>
2024-10-22 20:19:27 +0200

    created urls

---
+++ station/urls.py
@@ -0,0 +1,31 @@
+from django.urls.conf import include
+from rest_framework import routers
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from station.views import (
+    StationViewSet,
+    RouteViewSet,
+    CrewViewSet,
+    TrainTypeViewSet,
+    TrainViewSet,
+    JourneyViewSet,
+    TicketViewSet,
+    OrderViewSet,
+)
+
+app_name = "train-station-ap1"
+
+router = routers.DefaultRouter()
+router.register("stations", StationViewSet, basename="stations")
+router.register("routes", RouteViewSet, basename="routes")
+router.register("crews", CrewViewSet, basename="crews")
+router.register("train-types", TrainTypeViewSet, basename="train_types")
+router.register("train", TrainViewSet, basename="train")
+router.register("journeys", JourneyViewSet, basename="journeys")
+router.register("orders", OrderViewSet, basename="orders")
+router.register("tickets", TicketViewSet, basename="tickets")
+
+urlpatterns = [
+    path("", include(router.urls), name="station"),
+]
