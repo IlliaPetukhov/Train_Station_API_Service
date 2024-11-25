824a32baddd7564e41e2b97d43bcdb7864353d34
Illia <ilaruslanovich7@gmail.com>
2024-11-25 22:10:17 +0100

    added .env.sample and added django-secret-key to .env

--- Train_Station_API_Service/settings.py
+++ Train_Station_API_Service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qk!b5dccq06mj+m6qcibt_1$d7$bdul^_!l&=&fv(vwvom=9+#'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
